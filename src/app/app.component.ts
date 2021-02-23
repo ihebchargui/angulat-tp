dd4303a216315e66f0720aa67809dec24b5ad8e9
ihebchargui <iheb.chargui123@episousse.com.tn>
2021-02-23 09:25:36 +0100

    comm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Projet';
+  title = 'my-app';
+  imgUrl = "assets/images/che.jpg"
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { ContainerComponent } from './container/container.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  MatSliderModule} from '@angular/material/slider';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
